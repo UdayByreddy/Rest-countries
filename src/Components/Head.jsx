2ea9a31ca20dace0e1997c1900e76fe2ff39e73a
Uday kiran Reddy <udaykiranbyreddy@gmail.com>
2024-10-25 18:26:17 +0530

    feat:Head file added

---
+++ src/Components/Head.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon } from '@fortawesome/free-regular-svg-icons'
+import { faSun } from '@fortawesome/free-regular-svg-icons'
+
+
+const Head = ({click,submitHandler}) => {
+
+  return (
+    <>
+      <header className={`w-full flex justify-between p-4 ${click?'shadow-transparent':'shadow-xl'}  lg:p-6`}>
+        <span className="text-sm font-bold lg:text-xl">Where in the World</span>
+        <button onClick={submitHandler} className="border-none px-4 lg:px-8 lg:text-xl">
+          <FontAwesomeIcon icon={click ? faSun : faMoon} className="mr-3" />
+          {click ? 'Light' : 'Dark'}
+        </button>
+      </header>
+    </>
+  );
+};
+
+export default Head;
