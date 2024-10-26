fa85d05eee557fe7f876e41f341685c94a09416c
Uday kiran Reddy <udaykiranbyreddy@gmail.com>
2024-10-26 12:40:27 +0530

    fix: change in background color

--- src/Components/Head.jsx
+++ src/Components/Head.jsx
@@ -8,7 +8,7 @@ const Head = ({click,submitHandler}) => {
 
   return (
     <>
-      <header className={`w-full flex justify-between p-4 ${click?'shadow-transparent':'shadow-xl'}  lg:p-6`}>
+      <header className={`w-full flex justify-between p-4 ${click?'dark-theme-head':'shadow-xl'}  lg:p-6`}>
         <span className="text-sm font-bold lg:text-xl">Where in the World</span>
         <button onClick={submitHandler} className="border-none px-4 lg:px-8 lg:text-xl">
           <FontAwesomeIcon icon={click ? faSun : faMoon} className="mr-3" />
