b9300f68b70e8a8c36df4d6f897e381c6f7f7b05
Uday kiran Reddy <udaykiranbyreddy@gmail.com>
2024-10-26 17:45:22 +0530

    feat: hook usereducer used

--- src/Layouts/TitleSection.jsx
+++ src/Layouts/TitleSection.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState } from 'react';
+import React, {useContext, useReducer, useState } from 'react';
 import { Toggler } from '../App';
 import Allcountries from './Allcountries';
 import SearchInput from '../Components/SearchInput';
@@ -6,11 +6,36 @@ import FilterSelect from '../Components/FilterSelect';
 
 
 const TitleSection = ({ click }) => {
-    const [inputValue, setInputValue] = useState('');
-    const [option, setOption] = useState('');
-    const [population, setPopulation] = useState('');
-    const [area, setArea] = useState('');
-    const [subregion, setSubRegion] = useState('');
+    const initialState = {
+        inputValue: '',
+        region: '',
+        population: '',
+        area: '',
+        subregion: ''
+    };
+    function reducer(state, action) {
+        switch (action.type) {
+            case "input":
+                return { ...state, inputValue: action.payload };
+            case "region":
+                return { ...state, region: action.payload };
+            case "population":
+                return { ...state, population: action.payload };
+            case "area":
+                return { ...state, area: action.payload };
+            case "subregion":
+                return { ...state, subregion: action.payload };
+            default:
+                throw new Error("Unknown action type");
+        }
+    }
+
+    const [state,dispatch] = useReducer(reducer,initialState);
+    // const [inputValue, setInputValue] = useState('');
+    // const [option, setOption] = useState('');
+    // const [population, setPopulation] = useState('');
+    // const [area, setArea] = useState('');
+    // const [subregion, setSubRegion] = useState('');
     const { countries } = useContext(Toggler);
 
     const regionOptions = [
@@ -34,35 +59,35 @@ const TitleSection = ({ click }) => {
     
     const subRegionList = () => {
         return countries
-            .filter((country) => country.region === option) 
-            .map((country) => ({ label: country.name, value: country.name })); // Return objects for FilterSelect
+            .filter((country) => country.region === state.region) 
+            .map((country) => ({ label: country.name, value: country.name })); 
     };
 
     return (
         <>
         <div>
              <div className={`mt-4 flex flex-wrap justify-between mx-2 lg:mx-0 ${click ? 'dark-theme' : 'light-theme'}`}>
-                <SearchInput click={click} onChange={setInputValue} />
+                <SearchInput click={click} onChange={(e)=>dispatch({type:"input", payload:e.target.value})} />
 
-                <FilterSelect options={regionOptions} option={option} onChange={setOption} click={click} label="Filter By Region" />
-               {option && <FilterSelect 
+                <FilterSelect options={regionOptions} option={state.region} onChange={(e)=>dispatch({type:"region",payload:e.target.value})} click={click} label="Filter By Region" />
+               {state.region && <FilterSelect 
                     options={subRegionList()}
-                    option={subregion} 
-                    onChange={setSubRegion} 
+                    option={state.subregion} 
+                    onChange={(e)=>dispatch({type:'subregion',payload:e.target.value})} 
                     label="Filter by Subregion" 
                     click={click} 
                 />} 
             </div>
 
             <div className={`mt-4 flex flex-wrap justify-between mx-2 lg:mx-0 ${click ? 'dark-theme' : 'light-theme'}`}>
-                <FilterSelect options={populationOptions} option={population} onChange={setPopulation} click={click} label="Filter by Population" />
+                <FilterSelect options={populationOptions} option={state.population} onChange={(e)=>dispatch({type:'population',payload:e.target.value})} click={click} label="Filter by Population" />
 
-                <FilterSelect options={areaOptions} option={area} onChange={setArea} click={click} label="Filter by Area" />
+                <FilterSelect options={areaOptions} option={state.area} onChange={(e)=>dispatch({type:'area',payload:e.target.value})} click={click} label="Filter by Area" />
             </div>
         </div>
            
 
-                <Allcountries click={click} inputValue={inputValue} option={option} population={population} area={area} subregion={subregion} />
+                <Allcountries click={click} inputValue={state.inputValue} option={state.region} population={state.population} area={state.area} subregion={state.subregion} />
         </>
     );
 };
