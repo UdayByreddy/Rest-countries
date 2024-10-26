b62615106f33316300e9407f26402c42beb36968
Uday kiran Reddy <udaykiranbyreddy@gmail.com>
2024-10-26 12:43:33 +0530

    feat: product not found page added

---
+++ src/ErrorHandler/ProductNotFoundPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+export default function ProductNotFoundPage() {
+  return (
+    <div className='flex flex-col justify-center items-center'>
+         <img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/no-item-found-4372183-3626865.png"/>
+         <h3 className='font-semibold text-xl'>Country Not Found</h3>
+    </div>
+  )
+}
