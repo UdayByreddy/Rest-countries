216d3f5b7755700bbf8d9836a899fcf37f1135a2
Uday kiran Reddy <udaykiranbyreddy@gmail.com>
2024-10-26 12:40:57 +0530

    fix: change in background color

--- src/Components/SearchInput.jsx
+++ src/Components/SearchInput.jsx
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function SearchInput({click,onChange}) {
   return (
     <>
-      <div className="relative w-full sm:w-80 mx-2 flex justify-center items-center lg:ml-4">
+      <div className="relative w-full  sm:w-80 mx-2 flex justify-center items-center lg:ml-4">
         <FontAwesomeIcon
           icon={faSearch}
           className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400"
@@ -13,10 +13,10 @@ export default function SearchInput({click,onChange}) {
         <input
           type="text"
           placeholder="Search for Country"
-          className={`pl-14 pr-4 py-3 w-full border rounded-lg focus:outline-none ${
+          className={`pl-14 pr-4 py-3 w-full border-none shadow-2xl rounded-lg focus:outline-none ${
             click
-              ? "bg-black text-white border-gray-600"
-              : "bg-white text-black border-gray-300"
+              ? "dark-theme-head"
+              : "light-theme"
           }`}
           onChange={(e) => onChange(e.target.value)}
         />
