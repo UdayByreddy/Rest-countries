8158e41a7a5beb4fc456cf4a05c27ab835640b41
Uday kiran Reddy <udaykiranbyreddy@gmail.com>
2024-10-26 15:36:56 +0530

    fix:set height for image

--- src/ErrorHandler/ErrorPage.jsx
+++ src/ErrorHandler/ErrorPage.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Head from '../Components/Head'
 import { Link } from 'react-router-dom'
+import { Toggler } from '../App'
 export default function ErrorPage() {
+  const {click,submitHandler} = useContext(Toggler);
   return (
     <>
-    <Head />
-    <div className='w-full h-vh flex flex-col justify-center items-center'>
-        <img src="https://colorlib.com/wp/wp-content/uploads/sites/2/404-error-template-3.png" className='w-full h-50'/>
-        <Link to={'/'}>
+    <Head click={click} submitHandler={submitHandler}/>
+    <div className='w-full flex flex-col justify-center items-center'>
+    <Link to={'/'} className='py-4'>
         <button className=' p-4 bg-white-600 text-black rounded-sm font-semibold shadow-xl'>Go To Home</button>
         </Link>
+        <img src="https://colorlib.com/wp/wp-content/uploads/sites/2/404-error-template-3.png" className='w-[50%] h-[300px] object-contain'/>
 
     </div>
     </>
